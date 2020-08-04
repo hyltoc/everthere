353cfce902bfda1fdc643ccf2fc92fccd80ba033
tocker <hylton.tocker@here.com>
2020-08-04 10:43:33 +0300

    Step 1 - Create project structure

---
+++ everthere-assignment/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <UseCases message="Hello"/>
+  </div>
+</template>
+
+<script>
+import UseCases from './views/UseCases/UseCases.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UseCases
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ everthere-assignment/src/views/UseCases/UseCases.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="page-container">
+       {{ message }},  this is the use-case table placeholder
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UseCases',
+    props: {
+        message: String
+    }
+}
+</script>
+
+<style scoped>
+.page-container {
+    padding: 14px;
+    background-color: #f6f7f9;
+    height: 100vh;
+}
+</style>
